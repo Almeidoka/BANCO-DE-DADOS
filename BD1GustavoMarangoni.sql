88324f8a43a64fd53c32c85928cb810c73ff9e9a
Almeida <107077336+Almeidoka@users.noreply.github.com>
2022-09-05 17:45:09 -0300

    AULA#2

--- BD1GustavoMarangoni.sql
+++ BD1GustavoMarangoni.sql
@@ -5,11 +5,12 @@ CREATE DATABASE agcorp;
 USE agcorp;
 
 CREATE TABLE cliente(
-id INT PRIMARY KEY NOT NULL AUTO_INCREMENT
-,nome VARCHAR(200) NOT NULL
-,endereco VARCHAR(200) NOT NULL
-,cidade VARCHAR(200) NOT NULL
-,estado CHAR(2) NOT NULL
+id INT NOT NULL AUTO_INCREMENT
+,nome VARCHAR(200) NOT NULL UNIQUE
+,endereco VARCHAR(200) NOT NULL UNIQUE
+,ativo CHAR(1) NOT NULL DEFAULT 'S'
+,siglaestado CHAR(2) NOT NULL
+,data_cadastro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 -- ,cep CHAR(8) NOT NULL
 -- ,telefone CHAR(11) NOT NULL
 -- ,email VARCHAR(200) NOT NULL
@@ -18,8 +19,21 @@ id INT PRIMARY KEY NOT NULL AUTO_INCREMENT
 -- ,sexo VARCHAR(200) NOT NULL
 -- ,datanascimento INT NOT NULL
 -- ,CHECK (sexo IN ('M' , 'F'))
+,CONSTRAINT pk_estado PRIMARY KEY (id)
+,CONSTRAINT estado_ativo_deve_ser_S_ou_N CHECK (ativo IN ('S', 'N') )
 );
-INSERT INTO cliente (nome,endereco,cidade,estado) VALUES ('GUSTAVO','RODNEY-MESIAS','MORRO-AGUDO','SP');
-SELECT id,nome,endereco,cidade,estado FROM cliente;
+INSERT INTO cliente (nome,endereco,ativo,siglaestado) VALUES ('GUSTAVO','RODNEY-MESIAS','S','SP');
+SELECT id,nome,endereco,siglaestado FROM cliente;
 
+CREATE TABLE estado (
+id INT NOT NULL AUTO_INCREMENT
+,nome VARCHAR(200)  NOT NULL
+,ativo CHAR(1) NOT NULL DEFAULT 'S' 
+,data_cadastro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+,estado_id INT NOT NULL 
+,CONSTRAINT pk_cidade PRIMARY KEY (id)
+,CONSTRAINT fk_cidade_estado FOREIGN KEY (estado_id) REFERENCES estado (id)
+,CONSTRAINT cidade_ativo_deve_ser_S_ou_N CHECK (ativo IN ('S','N'))
+,CONSTRAINT cidade_unica UNIQUE(nome, estado_id)
+);
 
